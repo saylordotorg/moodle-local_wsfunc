77bff7ed382a6781d85a6557eef12b768ae0a6a6
jazinheira <jazinheira@users.noreply.github.com>
2016-04-11 15:50:39 -0400

    Refactor externallib
    Also, it actually works now.

--- externallib.php
+++ externallib.php
@@ -22,34 +22,27 @@ class local_get_visible_courses_external extends external_api {
         // get_visible_courses_parameters() always return an external_function_parameters(). 
         // The external_function_parameters constructor expects an array of external_description.
         return new external_function_parameters(
-                array('options' => new external_multiple_structure(
-                            new external_single_structure(
-                                array (
-                                    'recursive' => new external_value(PARAM_INT, 'recursive'),
-                                    'summary' => new external_value(PARAM_INT, 'summary'),
-                                    'coursecontacts' => new external_value(PARAM_INT, 'course contacts'),
-                                    'sort' => new external_value(PARAM_RAW, 'sort fields'),
-                                    'offset' => new external_value(PARAM_INT, 'offset'),
-                                    'limit' => new external_value(PARAM_INT, 'limit'),
-                                    'idonly' => new external_value(PARAM_INT, 'idonly')
-                                )
-                            )
-                ))
-        );
+            array('cat' => new external_value(PARAM_INT, "Category ID: Get visible courses in the category. (Note: This is recursive and will get courses in sub-categories)", VALUE_DEFAULT, 0))
+            );
     }
  
     /**
      * The function itself
      * @return string welcome message
      */
-    public static function get_visible_courses($options) {
- 
+    public static function get_visible_courses($cat) {
+
         //Parameters validation
         $params = self::validate_parameters(self::get_visible_courses_parameters(),
-                array('options' => $options));
- 
+                array('cat' => $cat));
 
-        $courses = coursecat::get_courses($options);
+        $options[recursive] = true;
+        $options[coursecontacts] = false;
+        $options[summary] = true;
+        $options[sort][idnumber] = 1;
+ 
+        $coursecat = coursecat::get($params['cat']);
+        $courselist = coursecat::get_courses($options);
 
         //Note: don't forget to validate the context and check capabilities
         // $context = context_course::instance($course->id, IGNORE_MISSING);
@@ -63,8 +56,26 @@ class local_get_visible_courses_external extends external_api {
         //         throw new moodle_exception('errorcoursecontextnotvalid', 'webservice', '', $exceptionparam);
         //     }
         //     require_capability('moodle/course:view', $context);
- 
-        return $courses;
+        foreach ($courselist as $course) {
+                $id = $course->__get('id');
+                $category = $course->__get('category');
+                $shortname = $course->__get('shortname');
+                $fullname = $course->__get('fullname');
+                $startdate = $course->__get('startdate');
+                $summary = $course->__get('summary');
+
+                $courses[$id] = array(
+                        'id' => $id,
+                        'category' => $category,
+                        'shortname' => $shortname,
+                        'fullname' => $fullname,
+                        'startdate' => $startdate,
+                        'summary' => $summary
+                        );
+        }
+        ksort($courses);
+        $result['courses'] = $courses;
+        return $result;
     }
  
     /**
@@ -72,9 +83,23 @@ class local_get_visible_courses_external extends external_api {
      * @return external_description
      */
     public static function get_visible_courses_returns() {
-        return new external_value(PARAM_TYPE, 'Array of visible courses.');
+        return new external_single_structure(
+            array(
+                'courses' => new external_multiple_structure( new external_single_structure(
+                    array(
+                        'id' => new external_value(PARAM_INT, 'course id'),
+                        'category' => new external_value(PARAM_INT, 'category id'),
+                        'shortname' => new external_value(PARAM_TEXT, 'course shortname'),
+                        'fullname' => new external_value(PARAM_RAW, 'course fullname'),
+                        'startdate' => new external_value(PARAM_ALPHANUM, 'course startdate', VALUE_OPTIONAL),
+                        'summary' => new external_value(PARAM_RAW, 'course summary', VALUE_OPTIONAL),
+                    ), 'information about one course')
+                )  
+            )
+        );
     }
- 
+
+
  
  
 }
